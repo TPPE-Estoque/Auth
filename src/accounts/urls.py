7abad1e32ce551cd18e066a26b3bb374a27e89de
DiegoCarlito <diego.carlito01@gmail.com>
2025-11-25 20:58:36 -0300

    feat: adicionar hard delete para o admin

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, UserDetailView
 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', RegisterView.as_view(), name='register'),
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,10 +1,15 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class UserDetailView(generics.RetrieveDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
