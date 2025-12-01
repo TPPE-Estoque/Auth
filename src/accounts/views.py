7f1030f0f1c11e9411af3dd787eb9cefb888a86c
DiegoCarlito <diego.carlito01@gmail.com>
2025-12-01 19:04:57 -0300

    feat: permite atualização do cadastro de usuário

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,15 +1,45 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny, IsAdminUser
-
+from rest_framework.permissions import AllowAny
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-class UserDetailView(generics.RetrieveDestroyAPIView):
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [AllowAny]
+
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+        
+        if 'is_active' in validated_data:
+            instance.is_active = validated_data['is_active']
+
+        instance.save()
+        return instance
